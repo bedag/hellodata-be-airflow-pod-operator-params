efe2a30cf6686fa9e58841a549acd64072c2c97b
Felix Neidhart <felix.neidhart@bedag.ch>
2025-10-29 16:34:03 +0100

    Add test DAG files

---
+++ tests/example_dag_advanced.py
@@ -0,0 +1,60 @@
+from datetime import timedelta
+from pendulum import datetime
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
+    KubernetesPodOperator,
+)
+from hellodata_be_airflow_pod_operator_params import (
+    get_pod_operator_params,
+    EphemeralVolume,
+)  # library import
+
+operator_params = get_pod_operator_params(
+    "alpine:latest",
+    namespace="al1-hellodata-productsdev",
+    secret_names=["superset-demo-env"],
+    configmap_names=["superset-demo-extra-config"],
+    cpus=0.5,
+    memory_in_Gi=0.5,
+    local_ephemeral_storage_in_Gi=1,
+    startup_timeout_in_seconds=10 * 60,
+    large_ephemeral_storage_volume=EphemeralVolume(
+        "my-storage", 5, "/app/large_ephemeral_storage", "block-nutanix"
+    ),
+    env_vars={"key": "value"},
+)
+
+operator_params["tolerations"] = [
+    {
+        "key": "io.bedag.ch/hypervisor",
+        "operator": "Equal",
+        "value": "nutanix",
+        "effect": "NoSchedule",
+    },
+]
+
+default_args = {
+    "owner": "airflow",
+    "depend_on_past": False,
+    "start_date": datetime(2025, 8, 1, tz="Europe/Zurich"),
+}
+
+with DAG(
+    dag_id="example_dag_advanced",
+    schedule="@once",
+    default_args=default_args,
+    max_active_runs=1,
+    dagrun_timeout=timedelta(minutes=60 * 5),
+) as dag:
+
+    my_task = KubernetesPodOperator(
+        **operator_params,
+        name="my_task",
+        task_id="my_task",
+        arguments=[
+            """
+    echo "I run on kubernetes and have the following env vars" &&
+    printenv
+    """
+        ],
+    )
---
+++ tests/example_dag_simple.py
@@ -0,0 +1,45 @@
+from datetime import timedelta
+from pendulum import datetime
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
+    KubernetesPodOperator,
+)
+from hellodata_be_airflow_pod_operator_params import (
+    get_pod_operator_params,
+)  # library import
+
+operator_params = get_pod_operator_params(
+    "alpine:latest",
+    namespace="al1-hellodata-productsdev",
+    cpus=8,
+    memory_in_Gi=10,
+    local_ephemeral_storage_in_Gi=6,
+    startup_timeout_in_seconds=10 * 60,
+    env_vars={"key": "value"},
+)
+
+default_args = {
+    "owner": "airflow",
+    "depend_on_past": False,
+    "start_date": datetime(2025, 8, 1, tz="Europe/Zurich"),
+}
+
+with DAG(
+    dag_id="example_dag_simple",
+    schedule="@once",
+    default_args=default_args,
+    max_active_runs=1,
+    dagrun_timeout=timedelta(minutes=60 * 5),
+) as dag:
+
+    my_task = KubernetesPodOperator(
+        **operator_params,
+        name="my_task",
+        task_id="my_task",
+        arguments=[
+            """
+echo "I run on kubernetes and have the following env vars" &&
+printenv
+"""
+        ],
+    )
