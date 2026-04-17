72e734115a5d450c71253d84ae194e1960d5c5c1
Felix Neidhart <felix.neidhart@bedag.ch>
2026-04-17 14:44:02 +0200

    Adapt test DAG to new API

--- tests/example_dag_simple.py
+++ tests/example_dag_simple.py
@@ -6,23 +6,25 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
 )
 
 # ##### REMOVE BETWEEN #####
-# import os
-# import sys
+import os
+import sys
 
-# current_file = os.path.abspath(__file__)
-# current_directory = os.path.dirname(current_file)
-# sys.path.append(current_directory)
+current_file = os.path.abspath(__file__)
+current_directory = os.path.dirname(current_file)
+sys.path.append(current_directory)
+from pod_operator_params import get_pod_operator_params  # local import
+
 # ##### REMOVE BETWEEN #####
-from hellodata_be_airflow_pod_operator_params import (
-    get_pod_operator_params,
-)  # library import
+# from hellodata_be_airflow_pod_operator_params import (
+#     get_pod_operator_params,
+# )  # library import
 
 operator_params = get_pod_operator_params(
-    "alpine:latest",
-    namespace="al1-hellodata-productsdev",
+    "artifacts.bedag.cloud/dockerhub/alpine:latest",
+    namespace="x0e-hellodata-test-bd01",
     cpus=8,
     memory_in_Gi=10,
-    local_ephemeral_storage_in_Gi=6,
+    storage_in_Gi=6,
     startup_timeout_in_seconds=10 * 60,
     env_vars={"key": "value"},
 )
